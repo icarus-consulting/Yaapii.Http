0cdcab8a89018cbc65a7dfa7141a8a1b2ec16902
Miro Tunk <miro.tunk@icarus-consult.de>
2018-04-23 09:34:54 +0200

    fix: timeout was broken
    closes #9

---
+++ tests/Test.Yaapii.Http/HttpRequestTests.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+using Yaapii.Http;
+using Yaapii.Http.Method;
+
+namespace Test.Yaapii.Http
+{
+    public sealed class HttpRequestTests
+    {
+        [Fact]
+        public void AcceptsTimeout()
+        {
+            Assert.True(
+                new HttpRequest(
+                    new Get(), 
+                    new Uri("http://www.github.com"), 
+                    new TimeSpan(0, 5, 0)
+                ).Response().Status() == 200
+            );
+        }
+    }
+}
