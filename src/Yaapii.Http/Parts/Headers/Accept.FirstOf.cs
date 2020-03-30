02d5ec3c4c36ed66fb312b62c77efc49e3bdfd40
dfu398 <dominik.fuchs@icarus-consult.de>
2020-03-30 19:14:04 +0200

    feat: add *.FirstOf header classes

---
+++ src/Yaapii.Http/Parts/Headers/Accept.FirstOf.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Yaapii.Http.AtomsTemp.Scalar;
+using Yaapii.Http.Facets;
+
+namespace Yaapii.Http.Parts.Headers
+{
+    public sealed partial class Accept
+    {
+        /// <summary>
+        /// First value of any "Accept" header field.
+        /// </summary>
+        public sealed class FirstOf : TextEnvelope
+        {
+            /// <summary>
+            /// First value of any "Accept" header field.
+            /// </summary>
+            public FirstOf(IDictionary<string, string> input) : base(() =>
+                new FirstOf<string>(
+                    new Accept.Of(input)
+               ).Value()
+            )
+            { }
+        }
+    }
+}
